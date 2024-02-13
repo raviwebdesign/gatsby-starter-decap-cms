562c971187ed23e7653b4bcd1d0a524376f83e86
raviwebdesign <93245115+raviwebdesign@users.noreply.github.com>
2024-02-13 12:34:15 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,7 @@ const Navbar = () => {
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="author" content="ThemeZaa" />
-    <style>header {margin-bottom: 80px;}</style>
+   
     <meta
       name="viewport"
       content="width=device-width,initial-scale=1.0,maximum-scale=1"
