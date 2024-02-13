24101bcd93140a2d2f3ac6fbe82367798eb59d03
raviwebdesign <93245115+raviwebdesign@users.noreply.github.com>
2024-02-13 11:24:28 +0530

    Update cms.js

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -10,6 +10,3 @@ import IndexPagePreview from './preview-templates/IndexPagePreview'
 CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
 
-CMS.registerPreviewTemplate('index', IndexPagePreview)
-CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
