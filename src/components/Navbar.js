3ae4ef824a85f9d849bfc0f82ed37ba6f8f6fe03
raviwebdesign <93245115+raviwebdesign@users.noreply.github.com>
2024-02-13 11:47:08 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,16 +8,6 @@ const Navbar = () => {
 
   return (
       <head>
-    
-<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-N42TX3Z');</script>
-  <!-- End Google Tag Manager -->
-  
-  
     <title>Vider Business Solutions</title>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
